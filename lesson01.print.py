ed0a5320b87f40eaba20d270c2bef91f1861a3e6
sirchance123 <chancehuxter@gmail.com>
2025-09-24 16:18:51 +0000

    i did some stuff lowkey

---
+++ lesson01.print.py
@@ -0,0 +1,21 @@
+# gosh im so skibidi
+print("I AM OHBABY, \nI AM OHBABY")
+print("i am \t OHBABY. \t my name is not.... \t OHBABY.  \t i am... the \t OHBABY")
+#i am ohbaby. six sayvan
+#integers
+print(67 + 41 / 21)
+#Float
+print(4.0)
+print(4.0 + 6.7)
+print(6.7 - 4.1)
+# BOOlean
+print(True - True)
+print(True + False)
+print(True * False)
+print(1 == 2)
+print(1 == 1)
+print(1 != 2)
+print("hello" == "hello")
+
+print("\n DATA TYPE LOOKUP \n")
+print(type("to see this message, wrap me in some bubbble gum type print"))
