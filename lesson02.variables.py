8b3923759e7a31dd387a406cb959bf55fae3c191
sirchance123 <chancehuxter@gmail.com>
2025-09-25 17:25:50 +0000

    i_learned_i_think

---
+++ lesson02.variables.py
@@ -0,0 +1,48 @@
+#variables hold value.  CHAT IS THIS REAL???
+#variables are held on PHYSICAL storage ona computer in some server in like prolly dubai or smth.
+ohbabay = "I AM OHBABY, I AM OHBABY" 
+
+password = "I AM THEE OHBABY"
+email = "OHBABYILOVEMONEY@BAYHARBOR"
+print("password", password)
+print("email", email)
+tempature = 72.5
+print(type(tempature))
+price = 3.99
+
+enabled = False
+print(enabled)
+is_complete = True
+print(is_complete)
+enabled = True
+print(enabled)
+
+
+#math
+x = 3.14
+y = 7
+print(x + y)
+count = 10
+print(count)
+count_down = count - 1
+print(count_down)
+count = count_down
+print(count)
+
+
+countt = 10
+countt_down = countt + 5
+print(countt_down)
+
+x = 4
+y = "hello"
+temporary = x
+y = 4
+x = "hello"
+print(y)
+print(x)
+#yeah this part didnt make sense lowkey
+its_raining = False
+its_raining = True
+print("Is it raining lowkey or highkey?", its_raining)
+print("that doesnt rlly make sense but okay broski")
