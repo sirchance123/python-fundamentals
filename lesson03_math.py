d763c64646d9d6e34addc4341fe013667d4b7bf9
sirchance123 <chancehuxter@gmail.com>
2025-09-26 18:01:22 +0000

    I AM OHBABY. i did math :(

---
+++ lesson03_math.py
@@ -0,0 +1,32 @@
+#hi guys so like this is math i think idrk.
+#key concepts: math operators: +, -, *, /, //, %, **
+add = 7 + 2
+print("Sum: ", add)
+subtract = 108 - 41
+print("Difference: ", subtract)
+multiply = 6 * 7
+print("Product: ", multiply)
+divide = 134 / 2
+print("Divsion: ", divide)
+float_divide = 134 / 2
+print("Float Divsion: ", float_divide)
+integer_divide = 134 // 2
+print("Integer Divsion: ", integer_divide)
+
+#modulus, more like...uhh...sp..spodulus.
+modulus = 6 % 7
+print("Modulus: ", modulus)
+exponent = 6 ** 7
+print("Exponent: ", exponent)
+print(2 + 2 != 10 - 6)
+
+result1 = (2 + 3) * 4
+print("Result 1: ", result1)
+result2 = 2 ** 3 * 4
+print("Refult 2: ", result2)
+result3 = 20 / 5 * 2
+print("Result 3:", result3)
+result4 = 10 - 2 + 3
+print("Result 4: ", result4)
+result5 = 5 + 2 ** 3 * (4 - 1)
+print("Result 5: ", result5)
