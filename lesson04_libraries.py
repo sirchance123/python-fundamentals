286b8f1055818bd89c2c8876f2862aa5f7948fd0
sirchance123 <chancehuxter@gmail.com>
2025-09-30 15:53:52 +0000

    adds math library examples

---
+++ lesson04_libraries.py
@@ -0,0 +1,9 @@
+#libraries are like stuff that hold stuff (i think.  i lowkey dont know)
+import math
+print(math.sqrt(25))
+print(math.ceil(4.2))
+print(math.floor(4.8))
+print(math.pow(2, 5))
+print(math.pi)
+
+
