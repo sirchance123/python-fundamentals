7af9e880cabb3c64a04decc1dd28f3cfef9ae39d
sirchance123 <chancehuxter@gmail.com>
2025-10-01 17:25:54 +0000

    i actually think im improving

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -11,4 +11,44 @@ random = math.pi
 
 psuedo = 718302
 part1 = psuedo - random
-part2
+
+
+
+
+print("------\n\n")
+
+
+seed = 12.4444
+step1 = seed / 6.7
+step2 = step1 - 800
+step3 = step2 + 180843
+step4 = step3 % 10
+result = math.floor(step4)
+print(result)
+
+
+
+
+
+import random
+print(random.randint(1, 7))
+print(random.random())
+print(random.choice(["egg", "milk", "cheese"]))
+my_fav = ["egg", "milk", "cheese"]
+print(random.choice(my_fav))
+random.shuffle(my_fav)
+print(my_fav)
+
+
+
+
+#challenge 1
+diameter = 14
+radius = diameter / 2
+circle_area = (radius ** 2) * math.pi
+print(circle_area)
+
+
+#challenge 2
+die_roll = random.randint(1, 6)
+print("You dice roll is...", die_roll)
