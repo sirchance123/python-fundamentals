d661fc73d1b8d32464c04f732a6587c92498522a
sirchance123 <chancehuxter@gmail.com>
2025-10-21 17:23:42 +0000

    i trtied the challlenges

---
+++ challenges.py
@@ -0,0 +1,31 @@
+#challenge real not fakke
+
+palindrone_check = input("Type a random word : ")
+reversed_palindrone_check=palindrone_check[::-1]
+if reversed_palindrone_check == palindrone_check :
+    print("This word is a palindrone")
+else :
+    print("This word is NOT a palindrone")
+
+#challenge 2
+
+email = input("enter your email.  MAKE SURE TO ADD AN @ : ")
+#idk man
+
+
+
+
+
+#challenge3
+
+given_last = ["Le Murr", "Le' Boin", "Shy Boy"]
+shy_boy = input("Enter a battle cats enemy, specifcally one that large and red, and is also a head : ")
+if shy_boy == given_last[2] : 
+    print("You are the smart cookie")
+else :
+    print("you bunz")
+
+
+#challenge vier (gemran 4)
+
+#im sorry but i have absolutely no idea and ive asked people for help but no one came.  :(
