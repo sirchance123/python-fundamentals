60f2d7195f86608828ffdfdee4ef81c8749f6bfd
sirchance123 <chancehuxter@gmail.com>
2025-10-24 15:32:22 +0000

    i did a loop

---
+++ lesson09_loops.py
@@ -0,0 +1,22 @@
+#two types of loops
+#for loop
+import time
+bums = ["Le Murr", "Le' Boin", "General Gre Gory", "Shadow Boxer K", "The Cat God" ]
+bums[0]
+
+for frauds in bums:
+    print("Now Eliminating : ", frauds)
+    time.sleep(1.5)
+
+
+print("All these frauds got cooked")
+
+
+
+
+
+
+
+for i in range(5):
+    print("Counting frauds...", i)
+
