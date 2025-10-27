e21e2668588b8064e57696f2ebcf64d5dc911048
sirchance123 <chancehuxter@gmail.com>
2025-10-27 17:02:23 +0000

    i did a loop part 2

--- lesson09_loops.py
+++ lesson09_loops.py
@@ -20,3 +20,37 @@ print("All these frauds got cooked")
 for i in range(5):
     print("Counting frauds...", i)
 
+
+
+fav_word = "six swan"
+letter_list = []
+for letter in fav_word:
+    print(letter, end="")
+letter_list.append(letter)
+print(letter_list)
+
+count = 0
+
+while count < 5: 
+    print(f"Loopin'. We are on loop # {count}.")
+    count += 1
+    time.sleep(0.5)
+
+print("We have escaped the loop!")
+
+user_input = ""
+
+while user_input != "exit":
+    user_input = input("Type 'exit' to escape:")
+
+
+
+
+count = 60
+increment = 1
+
+while count > 0:
+    count += increment
+    increment += 100000
+
+    print(count)
